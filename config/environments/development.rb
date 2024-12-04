621c3f60ab5c5cbb8b833b1bc5a9f4e7f7a5d3fd
r_u_0601 <rashmi.r.upadhya@gmail.com>
2024-12-04 11:39:31 +0530

    Added mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,8 +37,18 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
 
-  config.action_mailer.perform_caching = false
+   config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 '587',
+    user_name:            'visioninternational37@gmail.com',
+    password:             'mbwf mnek wtbl dmwl',
+    authentication:       :login
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  # Authenticated user routes
   authenticated :user do
     root 'blog_posts#dashboard', as: :authenticated_root
   end
 
-  # Public landing page
   root 'pages#home'
 
   resources :blog_posts do
